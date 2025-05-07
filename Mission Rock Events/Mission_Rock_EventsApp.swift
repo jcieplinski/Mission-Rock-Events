5d501f3f829533bc58f801d1bd0e53943c331765
Joe Cieplinski <joec@mac.com>
2025-05-07 10:15:01 -0700

    Initial Commit

---
+++ Mission Rock Events/Mission_Rock_EventsApp.swift
@@ -0,0 +1,32 @@
+//
+//  Mission_Rock_EventsApp.swift
+//  Mission Rock Events
+//
+//  Created by Joe Cieplinski on 5/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Mission_Rock_EventsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
